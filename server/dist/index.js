ff407da20e3c65cb466f9b258f220d77312133f2
Tim Dronkers <tdronkers@gmail.com>
2023-09-08 11:53:54 -0600

    adding eslint and prettier config

--- server/dist/index.js
+++ server/dist/index.js
@@ -8,8 +8,8 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server!!!");
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server!!!');
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
