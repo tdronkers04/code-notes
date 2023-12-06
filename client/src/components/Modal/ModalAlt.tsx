94b8925faa65b19c9bd870df90240e3ef48f7449
Tim Dronkers <tdronkers@gmail.com>
2023-12-06 06:36:46 -0700

    disable scrolling behind modal

--- client/src/components/Modal/ModalAlt.tsx
+++ client/src/components/Modal/ModalAlt.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 
 function Modal({
   children,
@@ -9,12 +9,20 @@ function Modal({
 }) {
   const nodeRef = useRef(null);
 
-  useEffect(() => {
-    const closeOnEscapeKey = (e: KeyboardEvent) =>
+  useLayoutEffect(() => {
+    const closeOnEscapeKey = (e: KeyboardEvent) => {
       e.key === 'Escape' ? handleClose() : null;
+    };
+
     document.body.addEventListener('keydown', closeOnEscapeKey);
+
+    // Disable scrolling behind modal
+    document.body.style.overflow = 'hidden';
+
     return () => {
       document.body.removeEventListener('keydown', closeOnEscapeKey);
+      // Re-enable scrolling
+      document.body.style.overflow = 'visible';
     };
   }, []);
 
@@ -23,7 +31,7 @@ function Modal({
       className="fixed inset-0 flex items-center justify-center z-50"
       ref={nodeRef}
     >
-      <div className="absolute inset-0 bg-zinc-900 bg-opacity-80"></div>
+      <div className="absolute inset-0 bg-zinc-800 bg-opacity-80"></div>
       <div className="relative">{children}</div>
     </div>
   );
